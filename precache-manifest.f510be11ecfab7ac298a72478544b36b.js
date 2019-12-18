792909cd73ac8311eb913665aac3ff6c0ceb7d96
unknown <fmmantelli@gmail.com>
2019-12-18 15:52:15 -0300

    Updates

---
+++ precache-manifest.f510be11ecfab7ac298a72478544b36b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "547986dfea2bb95708f1dc052d376644",
+    "url": "/sincroplan/index.html"
+  },
+  {
+    "revision": "868fddd8cf8d4beb6091",
+    "url": "/sincroplan/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d623c71de48f5f2ca14e",
+    "url": "/sincroplan/static/js/2.ec57ce60.chunk.js"
+  },
+  {
+    "revision": "b651d5533fc80509587379dd07cc06bd",
+    "url": "/sincroplan/static/js/2.ec57ce60.chunk.js.LICENSE"
+  },
+  {
+    "revision": "868fddd8cf8d4beb6091",
+    "url": "/sincroplan/static/js/main.328edb20.chunk.js"
+  },
+  {
+    "revision": "9a9b14210ef9e056911f",
+    "url": "/sincroplan/static/js/runtime-main.af06a6eb.js"
+  },
+  {
+    "revision": "307f2f245ef02277a1ff1610fef6ddaf",
+    "url": "/sincroplan/static/media/electrical.307f2f24.jpg"
+  }
+]);
