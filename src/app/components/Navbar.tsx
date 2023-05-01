7b6e14a77ec188d925ed10ad385dcfa91d2afed4
Fabio Mantelli <fmmantelli@gmail.com>
2023-04-30 22:21:23 -0300

    feat: black menu mobile

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,64 @@
+"use client";
+import { useState } from "react";
+
+import { Squash as Hamburger } from "hamburger-react";
+
+function Navbar() {
+  const [openMobile, setOpenMobile] = useState(false);
+
+  const handleMobile = () => {
+    setOpenMobile(!openMobile);
+    
+    if (openMobile) {
+      document.body.style.overflow = "unset";
+    } else {
+      document.body.style.overflow = "hidden";
+    }
+  };
+
+  return (
+    <div className="bg-[#F9F5EE] text-black z-10">
+      <hr className="border-none h-4 bg-black" />
+      <button
+        onClick={handleMobile}
+        className="md:hidden absolute z-10 right-1 top-8"
+      >
+        <Hamburger color={`${openMobile ? "#fff" : "#000"}`} />
+      </button>
+
+      <div
+        className={`flex items-center justify-center ${
+          openMobile
+            ? "bg-black translate-y-0 h-mobile"
+            : "-translate-y-0 h-20"
+        } bg-[#F9F5EE] transition-all duration-500 ease-out`}
+      >
+        <ul className={`flex gap-4`}>
+          <li className="absolute left-8 top-6 md:static">
+            SINCRO<strong>PLAN</strong>
+          </li>
+          <ul className="flex flex-col gap-4 md:flex-row">
+            <li className="hidden md:flex">QUEM SOMOS</li>
+            <li className="hidden md:flex">PROJETOS DESENVOLVIDOS</li>
+            <li className="hidden md:flex">SOFTWARES</li>
+            <li className="hidden md:flex">EXPLORAR</li>
+          </ul>
+        </ul>
+      </div>
+      <div>
+        <ul
+          className={`absolute top-28 left-8 flex flex-col gap-5 z-30 text-lg text-white ${
+            openMobile ? "animate-fade-in-down" : "invisible"
+          }`}
+        >
+          <li>QUEM SOMOS</li>
+          <li>PROJETOS DESENVOLVIDOS</li>
+          <li>SOFTWARES</li>
+          <li>EXPLORAR</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      height: {
+        'mobile': 'calc(100vh - 16px);'
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      keyframes: {
+        "fade-in-down": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(-10px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+      },
+      animation: {
+        "fade-in-down": "fade-in-down 0.500s ease-out",
       },
     },
   },
   plugins: [],
-}
+};
